293999470fc77265cbb545ff77386897309693c1
Jeremiah-Williams <jae4lani17@gmail.com>
2024-09-17 16:16:40 -0400

    second commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rootuser:Gitkoding@AX15:3306/regaloo'
-    SQLALCHMEY_TRACT_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rootuser:Gitkoding@AX15:3306/regaloo'
+    # SQLALCHMEY_TRACT_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
